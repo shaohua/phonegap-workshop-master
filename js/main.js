b675d801072fc9ea3ae5904f8da449674a4c392e
Shaohua Zhou <shao@versal.com>
2013-12-07 16:58:47 -0800

    Fix this bugs in the tutorial

--- js/main.js
+++ js/main.js
@@ -2,14 +2,9 @@ var app = {
 
     findByName: function() {
         console.log('findByName');
+        var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+            $('.employee-list').html( self.employeeLiTpl(employees) );
         });
     },
 
@@ -22,13 +17,9 @@ var app = {
     },
 
     renderHomeView: function() {
-        var html =
-                "<div class='header'><h1>Home</h1></div>" +
-                "<div class='search-view'>" +
-                "<input class='search-key'/>" +
-                "<ul class='employee-list'></ul>" +
-                "</div>";
-        $('body').html(html);
+        this.homeTpl = Handlebars.compile($('#home-tpl').html());
+        this.employeeLiTpl = Handlebars.compile($('#employee-li-tpl').html());
+        $('body').html( this.homeTpl() );
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
